f7121d97e56d70b9cd1fe9f0a62909cf866918e1
takuyaw-w <takuya.jamtb@gmail.com>
2023-01-02 03:46:53 +0900

    update

--- src/_data/site.ts
+++ src/_data/site.ts
@@ -1,6 +1,8 @@
-export const site = {
+const site = {
   title: "my-site-tile",
   lang: "ja" as "ja" | "en",
 } as const;
 
 export type Site = typeof site;
+
+export default site;
--- src/_includes/layouts/Base.tsx
+++ src/_includes/layouts/Base.tsx
@@ -1,4 +1,4 @@
-import { ComponentChildren } from "preact";
+import type { ComponentChildren } from "preact";
 import type { Site } from "../../_data/site.ts";
 import Head from "./Head.tsx";
 
